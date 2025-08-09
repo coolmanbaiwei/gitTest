2e938d699af4a2a0cdc71774d93c337d68deefa9
好 李 <3341229191@qq.com>
2025-08-09 18:44:59 +0800

    version -1.2 by jiafeimao

--- testGit/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ testGit/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,15 +1,29 @@
 package com.example.demo;
 
+import com.example.demo.domin.User;
+import com.example.demo.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class DemoApplicationTests {
+
+    @Autowired
+    private UserMapper userMapper;
 
     @Test
     void contextLoads() {
         System.out.println("hello Word!!");
         System.out.println("fuck");
+    }
+
+    @Test
+    void test1() {
+        List<User> users = userMapper.selectList(null);
+        System.out.println(users.toString());
     }
 
 }
